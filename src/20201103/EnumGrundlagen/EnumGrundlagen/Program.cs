05518b90e99f939cc1f035c9493a81bc3ac36ee0
freakmaster@Wifi <user@WIN10DEV>
2020-11-03 21:24:49 +0100

    enum .... methoden usw.

---
+++ src/20201103/EnumGrundlagen/EnumGrundlagen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumGrundlagen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enum ist praktisch wenn eine Variable nur bestimmte Werte haben soll .... wird als String gespeichert
+
+            ParticipantState state = ParticipantState.Krank;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("TEST");
+            // Consolen Farben sollten wie folgt immer reseted werden
+            Console.ResetColor();
+            Console.WriteLine("TEST");
+
+            if(state == ParticipantState.Krank)
+            {
+                Console.WriteLine("Der Typ ist krank");
+            }
+
+            Console.WriteLine($"State: {state}");
+
+            // praktisch ist.... wenn man switch schreibt und dann mit TAB weiter geht ... macht VStudio gleich den switch case block für alle möglichen Werte ... voll cool ! 
+
+            switch (state)
+            {
+                case ParticipantState.Anwesend:
+                    break;
+                case ParticipantState.Abwesend:
+                    break;
+                case ParticipantState.Krank:
+                    break;
+                case ParticipantState.Unbekannt:
+                    break;
+                case ParticipantState.Abgemeldet:
+                    break;
+                default:
+                    break;
+            }
+
+            //eigenes Snippet erstellen ... nur cw schreiben und dann mit TAB TAB ... kann via Tools -> Code Snippet Manager bearbeitet werden oder checken wo auf der Platte das Snippet liegt und anpassen
+            Console.WriteLine($"");
+
+        }
+    }
+}
---
+++ src/20201103/MethodenGL/MethodenGL/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodenGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+            DisplayHello();
+
+            DisplayColoredMessage("Hallo liebe Leute", ConsoleColor.Yellow);
+            DisplayColoredMessage("Hallo liebe Leute", ConsoleColor.Green);
+            DisplayColoredMessage("Hallo liebe Leute", ConsoleColor.Red);
+
+
+            //der Inhalt des übergebenen Wertes wird in eine Variable gepackt, die dann ausgegeben wird
+            double erg = CalculateWeight(15.5);
+
+            Console.WriteLine(erg);
+
+            string inputPrompt = string.Empty;
+
+
+            
+
+        }
+        
+        
+        //eine Methode besteht aus .... Rückgabetyp=void Bezeichnung=DisplayHello ( [Parameter] ) ... Parameter ist optional ... jeder Parameter hat einen Datentyp und eine Variable
+        //Signatur
+
+        //der graue Text sind Codelets ... 4 references bedeutet das 4x verwendet wurde ... super praktisch
+        static void DisplayHello()
+        {
+            Console.WriteLine("Hello!");
+
+
+        }
+
+        //Als Parameter werden die Message und die Farbe übergeben
+        static void DisplayColoredMessage(string message, ConsoleColor messageColor)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = messageColor;
+            
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+
+        }
+
+        //... Parameter müssen übergeben werden AAAABER  der Rückgabewert in diesem Fall result muss nicht verwendet werden
+        static double CalculateWeight(double nettoWeight)
+        {
+            double result = 0.0;
+
+            result = nettoWeight * 1.25;
+
+            // wenn eine Methode einen Wert zurückgeben soll bzw. einen Rückgabewert definieren, dann muss immer mit return zurückgegeben werden
+            return result;
+            
+        }
+
+
+        
+        
+        static int GetInt(string inputPrompt)
+        {
+            
+            string input = string.Empty;
+            bool inputIsValide = true;
+            int output = 0;
+
+            do
+            {
+                Console.Write("Bitte geben Sie einen Zahl ein: ");
+                input = Console.ReadLine();
+                inputIsValide = true;
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    inputIsValide = false;
+                    Console.WriteLine("Bitte machen Sie eine Eingabe!");
+                }
+                else
+                {
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (!char.IsDigit(input[i]))
+                        {
+                            inputIsValide = false;
+                            Console.WriteLine("Ungültige Eingabe ! Es dürfen nur Zahlen eingegeben werden !");
+                            break;
+                        }
+                    }
+                }
+            }
+            while (!inputIsValide);
+            output = int.Parse(input);
+            return output;
+            
+                                 
+        }
+        
+    }
+}
