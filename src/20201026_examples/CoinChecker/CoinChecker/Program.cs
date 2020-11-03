2e217de9f7dbb1dbe4ea73936c7be7bd548b3c6c
Freakmaster <Freakmaster@DESKTOP-7QU8QIQ>
2020-11-03 15:34:35 +0100

    coinchecker

---
+++ src/20201026_examples/CoinChecker/CoinChecker/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = 0;
+            string input = string.Empty;
+            bool inputIsValid = true;
+
+
+
+
+            do
+            {
+                // request amount from user
+                Console.Write("Bitte geben Sie einen gültigen Münzbetrag ein: ");
+                input = Console.ReadLine();
+
+                //check if input is valid...no empty strings 
+                inputIsValid = true;
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    inputIsValid = false;
+                }
+                else
+                {
+                    // check each char of the string whether it's a digit or not
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (!char.IsDigit(input[i]) || char.Equals(input[i], '.'))
+                        {
+                            inputIsValid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            while (!inputIsValid);
+
+                amount = double.Parse(input);
+
+                if (amount == 5 || amount == 10 || amount == 20 || amount == 50 || amount == 100 || amount == 200 )
+                {
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                }
+                else
+                {
+                    Console.WriteLine($"Der Betrag {amount} ist ungültig");
+                }
+            
+            Console.Beep(440, 1000);
+            Console.Beep(880, 500);
+            Console.Beep(1720, 500);
+            Console.Beep(3360, 1000);
+            Console.Beep(6720, 500);
+            Console.Beep(13440, 200);
+            Console.Beep(440, 100);
+
+
+
+        }
+    }
+}
---
+++ src/20201026_examples/CoinChecker2/CoinChecker2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinChecker2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = 0;
+            string input = string.Empty;
+            bool inputIsValide = true;
+
+
+            do
+            {
+                Console.Write("Bitte geben Sie einen Betrag in EURO ein: ");
+                input = Console.ReadLine();
+                inputIsValide = true;
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    inputIsValide = false;
+                    Console.WriteLine("Bitte machen Sie eine Eingabe!");
+                }
+                else
+                {
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (!char.IsDigit(input[i]))
+                        {
+                            inputIsValide = false;
+                            Console.WriteLine("Ungültige Eingabe ! Es dürfen nur Zahlen eingegeben werden !");
+                            break;
+                        }
+                    }
+                }
+            }
+            while (!inputIsValide);
+
+
+            if (inputIsValide)
+            {
+                amount = double.Parse(input);
+            }
+            else
+            {
+                Console.WriteLine("Bitte nur Zahlen eingeben");
+            }
+
+            switch (amount)
+            {
+                case 5:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                case 10:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                case 20:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                case 50:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                case 100:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                case 200:
+                    Console.WriteLine($"Der Betrag {amount} ist gültig");
+                    break;
+                default:
+                    Console.WriteLine($"Der Betrag {amount} ist nicht gültig");
+                    break;
+            }
+
+        }
+    }
+}
