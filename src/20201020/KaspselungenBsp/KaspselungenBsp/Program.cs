ab654e66540be60b6e861491af36c9dc66090e18
freakmaster <badzonkbad@gmail.com>
2020-11-19 07:56:49 +0100

    test

---
+++ src/20201020/KaspselungenBsp/KaspselungenBsp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaspselungenBsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Vehicle("Honda", new DateTime(2000,5,1), 2000.00m);
+        }
+    }
+
+}
---
+++ src/20201020/KaspselungenBsp/KaspselungenBsp/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KaspselungenBsp
+{
+    internal class Vehicle
+    {
+        private string _marke;
+        private DateTime _baujahr;
+        private decimal _preis;
+
+        public Vehicle(string marke, DateTime baujahr, decimal preis)
+        {
+            _marke = marke;
+            _baujahr = baujahr;
+            _preis = preis;
+        }
+
+
+        public string Marke
+        {
+            get { return _marke; }
+        }
+
+
+    }
+}
